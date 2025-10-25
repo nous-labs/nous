31e4e59b07fc5b8536d36d82737d76a385af9524
alez04 <alez2106x@gmail.com>
2025-10-25 17:53:12 +0300

    hotfix: docs build

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,7 +1,8 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
+  // @ts-ignore weird error
   return {
     ...defaultMdxComponents,
     ...components,
